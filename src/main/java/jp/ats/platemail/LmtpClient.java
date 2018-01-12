83aa048867369d776172bb33c6e3816d2c5cab53
ats.t.chiba <ats.t.chiba@gmail.com>
2018-01-12 19:02:31 +0900

    modify

--- src/main/java/jp/ats/platemail/LmtpClient.java
+++ src/main/java/jp/ats/platemail/LmtpClient.java
@@ -30,53 +30,56 @@ public class LmtpClient {
 			BufferedReader reply = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			PrintStream send = new PrintStream(sock.getOutputStream())) {
 
-			readLine(reply, 220);
+			readReply(reply, 220);
 
 			send.print("LHLO " + host + CRLF);
 			send.flush();
 
-			//行数不明なのでそのまま読むとブロックされてしまう
-			//処理方法を考える
-			readLine(reply, 250);
-			readLine(reply, 250);
-			readLine(reply, 250);
-			readLine(reply, 250);
+			while (readReply(reply, 250));
 
 			send.print("MAIL FROM:<" + sender + ">" + CRLF);
 			send.flush();
 
-			readLine(reply, 250);
+			readReply(reply, 250);
 
 			send.print("RCPT TO:<" + recipient + ">" + CRLF);
 			send.flush();
 
-			readLine(reply, 250);
+			readReply(reply, 250);
 
 			send.print("DATA" + CRLF);
 			send.flush();
 
-			readLine(reply, 354);
+			readReply(reply, 354);
 
-			//本文のCRLF.を変換して書き込み
+			/*
+			 * 
+			 * 本文のCRLF.を変換して書き込み
+			 * 
+			 */
 
 			send.print(CRLF);
 			send.print(".");
 			send.print(CRLF);
 			send.flush();
 
-			readLine(reply, 250);
+			readReply(reply, 250);
 
 			send.print("QUIT" + CRLF);
 			send.flush();
 
-			readLine(reply, 221);
+			readReply(reply, 221);
 		}
 	}
 
-	private void readLine(BufferedReader reader, int successCode) throws IOException {
+	private boolean readReply(BufferedReader reader, int successCode) throws IOException {
+		String code = String.valueOf(successCode);
+		int continueMarkIndex = code.length();
 		String line = reader.readLine();
-		if (!line.startsWith(String.valueOf(successCode))) {
+		if (!line.startsWith(code)) {
 			throw new ProtocolException(line);
 		}
+
+		return line.charAt(continueMarkIndex) == '-';
 	}
 }
