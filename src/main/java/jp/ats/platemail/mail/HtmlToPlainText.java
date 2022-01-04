301e11a56723997e78bf480d594ea5959af2bc6c
ats.tchiba <ats.t.chiba@gmail.com>
2022-01-05 01:20:51 +0900

    update

--- src/main/java/jp/ats/platemail/mail/HtmlToPlainText.java
+++ src/main/java/jp/ats/platemail/mail/HtmlToPlainText.java
@@ -1,6 +1,6 @@
 package jp.ats.platemail.mail;
 
-import org.jsoup.helper.StringUtil;
+import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
